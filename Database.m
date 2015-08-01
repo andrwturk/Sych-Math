ca13621e18e7d2cfac802c474773392a3fba59fb
Mikhail Katliar <cepstr@gmail.com>
2015-08-01 14:10:13 +0200

    * Fixed some errors in the Database implementation. * Implemented reading audio data for a given Fragment.Id * Added 2 records to the Fragments table.

--- Database.m
+++ Database.m
@@ -1,6 +1,5 @@
 classdef Database < handle
-    %UNTITLED Summary of this class goes here
-    %   Detailed explanation goes here
+    % Provides access to the recordings and metadata.
     
     properties (Access = private)
         % matfile containing the tables.
@@ -16,7 +15,7 @@ classdef Database < handle
         function this = Database()
             this.mf = matfile('data/database.mat', 'Writable', true);
             
-            if ~isfield(this.mf, 'Fragment')
+            if isempty(who(this.mf, 'Fragment'))
                 t.Id = [];
                 t.Setup = [];
                 t.FileName = [];
@@ -31,6 +30,9 @@ classdef Database < handle
             end
         end
         
+        %
+        % Append a Fragment record to the database.
+        %
         function append(this, record)
             % Determine new record(s)' Id.
             if height(this.Fragment) > 0
@@ -41,10 +43,30 @@ classdef Database < handle
             
             for i = 1 : numel(record)
                 record(i).Id = start_id + i;
+                
+                for f = fieldnames(record)'
+                    if ischar(record(i).(f{1}))
+                        record(i).(f{1}) = {record(i).(f{1})};
+                    end
+                end
             end
         
             % Insert new record(s).
             this.Fragment = [this.Fragment; struct2table(record)];
+        end
+        
+        % Get audio data for a given Fragment.Id
+        function [data, fs] = getAudioData(this, fragment_id)
+            rec = this.Fragment(this.Fragment.Id == fragment_id, :);
+            if height(rec) > 1
+                error('Multiple fragments with Id=%d found! The database is in an inconsistent state!', fragment_id);
+            end
+            
+            if height(rec) < 1
+                error('No Fragment with Id=%d found.', fragment_id);
+            end
+            
+            [data, fs] = read_data(rec.FileName{1}, [rec.FragmentBegin + 1, rec.FragmentEnd]);
         end
         
         function val = get.Fragment(this)
--- read_data.m
+++ read_data.m
@@ -1,21 +1,35 @@
-function [data, fs] = read_data( filename )
+function [data, fs] = read_data( filename, samples )
     % Read data file and return audio samples array.
     % 1st dimension of data is a channes, 2ns is time.
     
     [~, ~, ext] = fileparts(filename);
     
+    if nargin < 2
+        samples = [1, Inf];
+    end
+    
     if strcmp(ext, '.wav')
-        [data, fs] = audioread(filename);
+        [data, fs] = audioread(filename, samples);
         data = data.';
     else
         n_chan = 3;
         fs = 51000;
         n_bits = 12;
+        bytes_per_sample = n_chan * 2;
 
         f = fopen(filename, 'r');
         cu = onCleanup(@() fclose(f));
+        
+        % Determine how many samples to read and seek to the starting
+        % position depending of the start and end sample.
+        if nargin > 1
+            fseek(f, (samples(1) - 1) * bytes_per_sample, 'bof');
+            samples_to_read = samples(2) - samples(1) + 1;
+        else
+            samples_to_read = Inf;
+        end
 
-        data = fread(f, [n_chan, Inf], 'int16', 0, 'l');
+        data = fread(f, [n_chan, samples_to_read], 'int16', 0, 'l');
         data = data / 2^(n_bits - 1) - 1;
     end
 end
